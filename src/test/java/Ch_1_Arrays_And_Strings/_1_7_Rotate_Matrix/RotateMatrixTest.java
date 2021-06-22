1176b6dccc21f2a7f620ae9414bf37ed000a0eb0
marek-sz <marekszymkowiak1@gmail.com>
2021-06-22 13:22:40 +0200

    1.7 Rotate Matrix by 90degrees (clockwise) couple test cases

--- src/test/java/Ch_1_Arrays_And_Strings/_1_7_Rotate_Matrix/RotateMatrixTest.java
+++ src/test/java/Ch_1_Arrays_And_Strings/_1_7_Rotate_Matrix/RotateMatrixTest.java
@@ -10,5 +10,14 @@ class RotateMatrixTest {
         int[][] one = {{1, 2}, {3, 4}};
         int[][] two = {{3, 1}, {4, 2}};
         assertArrayEquals(two, RotateMatrix.rotate(one));
+
+        one = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        two = new int[][]{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        assertArrayEquals(two, RotateMatrix.rotate(one));
+
+        one = new int[][]{{1}};
+        two = new int[][]{{1}};
+        assertArrayEquals(two, RotateMatrix.rotate(one));
     }
+
 }
